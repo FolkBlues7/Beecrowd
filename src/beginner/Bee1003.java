4469f5bc467b9b00a082f7ec77a32b4206b336a2
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2025-11-28 14:54:54 -0300

    1003 - Simple Sum

---
+++ src/beginner/Bee1003.java
@@ -0,0 +1,19 @@
+package beginner;
+import java.util.Scanner;
+//Simple Sum 1003
+public class Bee1003 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        
+        int A =  scanner.nextInt();
+        int B =  scanner.nextInt();
+        
+        System.out.printf("SOMA = %d%n",A+B);
+        
+        scanner.close();
+
+	}
+
+}
