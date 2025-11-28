5302c663c8806239bbd8b822d88a3d50cf80d917
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2025-11-28 16:26:24 -0300

    1006 - Average 2

---
+++ src/beginner/Bee1006.java
@@ -0,0 +1,21 @@
+package beginner;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Bee1006 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+
+		double A = scanner.nextDouble();
+		double B = scanner.nextDouble();
+		double C = scanner.nextDouble();
+
+		double weightedAverage = (A * 2 + B * 3 + C * 5) / 10;
+
+		System.out.printf("MEDIA = %.1f%n", weightedAverage);
+
+		scanner.close();
+	}
+}
