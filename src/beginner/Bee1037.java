c38dcacd57f70c0c91b3d0bcc86321073c0be612
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2025-11-30 21:49:29 -0300

    1037 - Interval FIX adding interval (50, 75]

--- src/beginner/Bee1037.java
+++ src/beginner/Bee1037.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 
 public class Bee1037 {
 
-	public static void main(String[] args) throws IOException {
-		Scanner scanner = new Scanner(System.in);
+	public class Main {
 
-		double number = scanner.nextDouble();
+		public static void main(String[] args) throws IOException {
+			Scanner scanner = new Scanner(System.in);
 
-		if (number < 0 || number > 100) {
-			System.out.println("Fora de intervalo");
-		} else if (number >= 0 && number <= 25) {
-			System.out.println("Intervalo [0,25]");
-		} else if (number > 25 && number <= 50) {
-			System.out.println("Intervalo (25,50]");
-		} else {
-			System.out.println("Intervalo (75,100]");
+			double number = scanner.nextDouble();
+
+			if (number < 0 || number > 100) {
+				System.out.println("Fora de intervalo");
+			} else if (number <= 25) {
+				System.out.println("Intervalo [0,25]");
+			} else if (number <= 50) {
+				System.out.println("Intervalo (25,50]");
+			} else if (number <= 75) {
+				System.out.println("Intervalo (50,75]");
+			} else {
+				System.out.println("Intervalo (75,100]");
+			}
+			scanner.close();
 		}
-
-		scanner.close();
-
 	}
-
 }
