bcc1f77111d05bcbcf2139ea960f9f0355ccdfa7
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2026-02-07 09:40:10 -0300

    1095 - Sequence IJ 1

---
+++ src/beginner/Bee1095.java
@@ -0,0 +1,23 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Bee1095 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int a = 1;
+		int b = 60;
+		// I=1 J=60
+		for (int i = 0; i < 13; i++) {
+			System.out.printf("I=%d J=%d%n", a, b);
+			a += 3;
+			b -= 5;
+
+		}
+
+		scanner.close();
+	}
+
+}
