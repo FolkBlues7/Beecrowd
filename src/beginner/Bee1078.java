9eeba87f3e6278efba383804ffe599023a727086
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2026-02-07 07:55:21 -0300

    1078 - Multiplication Table

---
+++ src/beginner/Bee1078.java
@@ -0,0 +1,20 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Bee1078 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		final int n = scanner.nextInt();
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 1; i <= 10; i++) {
+			result.append(i + " x ").append(n + " = ").append(n * i).append("\n");
+		}
+		System.out.print(result);
+
+	}
+
+}
