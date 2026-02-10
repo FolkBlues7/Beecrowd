a2e0a2f6404c4e3671f555d2a06f5e1c3e8d8234
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2026-02-10 14:44:07 -0300

    1097 - Sequence IJ 3

---
+++ src/beginner/Bee1097.java
@@ -0,0 +1,15 @@
+package beginner;
+
+public class Bee1097 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 9; i += 2) {
+			for (int j = 7; j >= 5; j--) {
+				System.out.printf("I=%d J=%d%n", i, j + i - 1);
+			}
+		}
+
+	}
+
+}
