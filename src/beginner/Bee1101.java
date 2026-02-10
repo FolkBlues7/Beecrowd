7ced53c39a1afd668131a72dae326df21a78c4dc
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2026-02-10 16:04:29 -0300

    1101 - Sequence of Numbers and Sum

--- src/beginner/Bee1101.java
+++ src/beginner/Bee1101.java
@@ -7,28 +7,26 @@ public class Bee1101 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
+		while (true) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
 
-		do {
+			if (a <= 0 || b <= 0) {
+				break;
+			}
+
 			int min = Math.min(a, b);
 			int max = Math.max(a, b);
-
 			int sum = 0;
 
 			for (int i = min; i <= max; i++) {
 				sum += i;
-				System.out.printf("%d ", i);
+				System.out.print(i + " ");
 			}
-			System.out.printf("Sum=%d%n", sum);
-
-			a = scanner.nextInt();
-			b = scanner.nextInt();
-
-		} while (a > 1 && b > 1);
+			System.out.println("Sum=" + sum);
+		}
 
 		scanner.close();
-
 	}
 
 }
