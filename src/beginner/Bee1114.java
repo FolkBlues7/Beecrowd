0ebae8c623d46bc0b041fdfc985ec63ade29edb0
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2026-02-11 15:47:04 -0300

    1114 - Fixed Password

---
+++ src/beginner/Bee1114.java
@@ -0,0 +1,28 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Bee1114 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+
+			int password = scanner.nextInt();
+
+			if (password == 2002) {
+				System.out.println("Acesso Permitido");
+				break;
+			} else {
+				System.out.println("Senha Invalida");
+			}
+
+		}
+
+		scanner.close();
+
+	}
+
+}
