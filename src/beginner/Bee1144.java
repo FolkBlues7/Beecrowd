0aeb5adbc0d10567685096dc6a372b49d4fe41b8
Vinicius Gabriel <viniciusgabrielprofissional42@gmail.com>
2026-02-13 15:56:03 -0300

    1144 - Logical Sequence

---
+++ src/beginner/Bee1144.java
@@ -0,0 +1,21 @@
+package beginner;
+
+import java.util.Scanner;
+
+public class Bee1144 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			System.out.printf("%d %d %d%n", i, i * i, i * i * i);
+			System.out.printf("%d %d %d%n", i, i * i + 1, i * i * i + 1);
+		}
+
+		scanner.close();
+
+	}
+
+}
